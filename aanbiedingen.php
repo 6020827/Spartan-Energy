01abfad1327bb09921c285663a509e8fe96f3cc7
Bjorn04 <6021946@mborijnland.nl>
2022-06-29 09:34:50 +0200

    code netjes

--- aanbiedingen.php
+++ aanbiedingen.php
@@ -152,7 +152,7 @@ function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
 
-/ Close when someone clicks on the "x" symbol inside the overlay */
+/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
